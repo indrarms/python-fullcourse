dd6f727c414655d45948cd364c95b27f98036a27
indrarms <indraramdani9113@gmail.com>
2026-02-27 10:02:10 +0700

    Python - Variables

---
+++ [2] pythonTypecasting/main.py
@@ -0,0 +1,9 @@
+# Typecasting = Converting one data type to another data type
+# string = str(), integer = int(), float = float(), boolean = bool()
+
+name = "Indra Ramdani Saputra"
+age = 21
+gpa = 3.5
+is_student = True
+
+print(type(name)) # Output: <class 'str'>
